9e51d18e635d7b953df04493fbbf7630fe50d5c8
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-12 13:40:27 +0100

    Added Sellers page

--- src/seller.java
+++ src/seller.java
@@ -1,9 +1,8 @@
-import java.awt.*;
+
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
 
-public class products implements ActionListener{
-
+public class seller {
     JFrame frame;
     JPanel panel1, panel2;
     JLabel nameOfSupermarket, iconLabel, manageLabel, productId, name, quantity, price, category, productsList;
@@ -25,8 +24,10 @@ public class products implements ActionListener{
   
     JTable productsListTable;
 
-    products(){
-        frame = new JFrame("Products Page");
+
+    seller(){
+
+        frame = new JFrame("Seller's Page");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1300, 700);
         frame.setLocation(20, 20);
@@ -51,12 +52,12 @@ public class products implements ActionListener{
         iconLabel.setIcon(new ImageIcon("src/shopcart.png"));
         iconLabel.setBounds(20, 150, 100, 100);
 
-        manageLabel = new JLabel("INVENTORY");
+        manageLabel = new JLabel("SELLER'S DASHBOARD");
         manageLabel.setForeground(Color.RED);
-        manageLabel.setBounds(380, 20, 400, 50);
+        manageLabel.setBounds(380, 20, 500, 50);
         manageLabel.setFont(new Font(null, Font.BOLD, 30));
- 
-        productId = new JLabel("PRODUCT ID");
+
+        productId = new JLabel("SELLER ID");
         productId.setForeground(Color.RED);
         productId.setBounds(40, 80, 100, 30);
         productId.setFont(new Font(null, Font.BOLD, 15));
@@ -76,7 +77,7 @@ public class products implements ActionListener{
 
         //quantity
 
-        quantity = new JLabel("QUANTITY");
+        quantity = new JLabel("PASSWORD");
         quantity.setForeground(Color.RED);
         quantity.setBounds(550, 80, 200, 30);
         quantity.setFont(new Font(null, Font.BOLD, 15));
@@ -86,69 +87,26 @@ public class products implements ActionListener{
         quantityTf.setBounds( 660, 80, 200, 30);
 
         //price
-        price = new JLabel("PRICE");
-        price.setForeground(Color.RED);
-        price.setBounds(550, 120, 200, 30);
-        price.setFont(new Font(null, Font.BOLD, 15));
+        // price = new JLabel("PRICE");
+        // price.setForeground(Color.RED);
+        // price.setBounds(550, 120, 200, 30);
+        // price.setFont(new Font(null, Font.BOLD, 15));
 
 
-        priceTf = new JTextField();
-        priceTf.setBounds( 660, 120, 200, 30);
+        // priceTf = new JTextField();
+        // priceTf.setBounds( 660, 120, 200, 30);
 
         //category
 
-        category = new JLabel("CATEGORY");
+        category = new JLabel("GENDER");
         category.setForeground(Color.RED);
-        category.setBounds(40, 160, 200, 30);
+        category.setBounds(550, 120, 200, 30);
         category.setFont(new Font(null, Font.BOLD, 15));
 
 
-        categoryCombo = new JComboBox<>(new Object[]{"  FoodStuff", "  Beverage", "  Clothing", "  Oil", "  Perfumes"});
-        categoryCombo.setBounds( 170, 160, 200, 30);
+        categoryCombo = new JComboBox<>(new Object[]{"  Male", "  Female"});
+        categoryCombo.setBounds( 660, 120, 200, 30);
         categoryCombo.setForeground(Color.RED);
-
-        add = new JButton("ADD");
-        add.setForeground(Color.WHITE);
-        add.setBackground(Color.RED);
-        add.setBounds( 290, 250, 80, 30);
-        add.setFont(new Font(null, Font.BOLD, 15));
-        add.addActionListener(this);
-
-
-        edit = new JButton("EDIT");
-        edit.setForeground(Color.WHITE);
-        edit.setBackground(Color.RED);
-        edit.setBounds( 400, 250, 80, 30);
-        edit.setFont(new Font(null, Font.BOLD, 15));
-
-
-        delete = new JButton("DELETE");
-        delete.setForeground(Color.WHITE);
-        delete.setBackground(Color.RED);
-        delete.setBounds( 500, 250, 100, 30);
-        delete.setFont(new Font(null, Font.BOLD, 15));
-
-
-        clear = new JButton("CLEAR");
-        clear.setForeground(Color.WHITE);
-        clear.setBackground(Color.RED);
-        clear.setBounds( 630, 250, 100, 30);
-        clear.setFont(new Font(null, Font.BOLD, 15));
-
-
-        //products Lists
-        productsList = new JLabel("PRODUCTS LIST");
-        productsList.setBounds(380, 290, 400, 50);
-        productsList.setFont(new Font(null, Font.BOLD, 25));
-        productsList.setForeground(Color.RED);
-
-
-        productsListTable = new JTable(data, columns);  
-        productsListTable.setFillsViewportHeight(true);
-        //productsListTable.setBounds(100, 370, 300, 200);  
-        JScrollPane scrollTable = new JScrollPane(productsListTable);   
-        scrollTable.setBounds(40,360, 900, 80); 
-        scrollTable.setFont(new Font(null, Font.BOLD, 20));
 
 
         panel1.add(nameOfSupermarket);   
@@ -171,7 +129,7 @@ public class products implements ActionListener{
         panel2.add(delete);
         panel2.add(clear);
         panel2.add(productsList);
-        panel2.add(scrollTable);
+        //panel2.add(scrollTable);
        //panel2.add(productsListTable);
         
     
@@ -180,15 +138,6 @@ public class products implements ActionListener{
 
         frame.setVisible(true);
 
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        if(e.getSource() ==add){
-            seller sellers = new seller();
-        }
-        
     }
     
 }
